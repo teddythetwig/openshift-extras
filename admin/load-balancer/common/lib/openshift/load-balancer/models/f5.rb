0b102d8221f7f7c946c0848e81aaf891fe79fea2
Miciah Masters <miciah.masters@gmail.com>
2013-06-24 23:24:42 +0000

    Move "/Common/" partition prefix to F5 code
    Instead of including "/Common/" in the pool and route names the daemon
    gives to the F5 controller, have the F5 model append the "/Common/"
    prefix.
    
    The "/Common/" prefix identifies the partition in which the pool or
    route is created, and because the concept of a partition is F5-specific,
    the prefix logically belongs in the F5-specific code.  LBaaS does not
    like the prefix, and so we must not include it in the general code.

--- admin/load-balancer/common/lib/openshift/load-balancer/models/f5.rb
+++ admin/load-balancer/common/lib/openshift/load-balancer/models/f5.rb
@@ -10,15 +10,15 @@ module OpenShift
   class F5LoadBalancerModel < LoadBalancerModel
 
     def get_pool_names
-      @bigip['LocalLB.Pool'].get_list
+      @bigip['LocalLB.Pool'].get_list.map {|pool| pool[8..-1]}
     end
 
     def create_pools pool_names, monitor_names
-      @bigip['LocalLB.Pool'].create pool_names, ['LB_METHOD_ROUND_ROBIN'], []
+      @bigip['LocalLB.Pool'].create pool_names.map {|pool| "/Common/#{pool}"}, ['LB_METHOD_ROUND_ROBIN'], []
       @bigip['LocalLB.Pool'].set_monitor_association pool_names.zip(monitor_names).map { |pool,monitor|
         if monitor
           {
-            :pool_name => pool,
+            :pool_name => "/Common/#{pool}",
             :monitor_rule => {
               :type => 'MONITOR_RULE_TYPE_SINGLE',
               :quorum => 1,
@@ -27,7 +27,7 @@ module OpenShift
           }
         else
           {
-            :pool_name => pool,
+            :pool_name => "/Common/#{pool}",
             :monitor_rule => {
               :type => 'MONITOR_RULE_TYPE_NONE',
               :quorum => 1,
@@ -39,30 +39,30 @@ module OpenShift
     end
 
     def delete_pools pool_names
-      @bigip['LocalLB.Pool'].delete_pool pool_names
+      @bigip['LocalLB.Pool'].delete_pool pool_names.map {|pool| "/Common/#{pool}"}
     end
 
     def get_route_names
-      @bigip['LocalLB.ProfileHttpClass'].get_list
+      @bigip['LocalLB.ProfileHttpClass'].get_list.map {|pool| pool[8..-1]}
     end
 
     def get_active_route_names
-      @bigip['LocalLB.VirtualServer'].get_httpclass_profile(['ose-vlan'])[0].map{|profile| profile.profile_name}
+      @bigip['LocalLB.VirtualServer'].get_httpclass_profile(['ose-vlan'])[0].map{|profile| profile.profile_name[8..-1]}
     end
 
     def create_routes pool_names, routes
       route_names, paths = routes.transpose
       priority = @bigip['LocalLB.VirtualServer'].get_httpclass_profile(['ose-vlan'])[0].map{|pri| pri.priority}.max || 0
-      @bigip['LocalLB.ProfileHttpClass'].create route_names
-      @bigip['LocalLB.ProfileHttpClass'].add_path_match_pattern route_names, paths.map {|path| [{:pattern=>"#{path}(/.*)?", :is_glob=>false}]}
-      @bigip['LocalLB.ProfileHttpClass'].set_pool_name route_names, pool_names.map{|name| {:value=>name, :default_flag=>false}}
-      @bigip['LocalLB.ProfileHttpClass'].set_rewrite_url route_names, paths.map{|path| {:value=>"[string map { \"#{path}\" \"/\" } [HTTP::uri]]", :default_flag=>false}}
-      @bigip['LocalLB.VirtualServer'].add_httpclass_profile ['ose-vlan'], [route_names.map {|name| {:profile_name=>name, :priority=>(priority += 1)}}]
+      @bigip['LocalLB.ProfileHttpClass'].create route_names.map {|name| "/Common/#{name}"}
+      @bigip['LocalLB.ProfileHttpClass'].add_path_match_pattern route_names.map {|name| "/Common/#{name}"}, paths.map {|path| [{:pattern=>"#{path}(/.*)?", :is_glob=>false}]}
+      @bigip['LocalLB.ProfileHttpClass'].set_pool_name route_names.map {|name| "/Common/#{name}"}, pool_names.map{|name| {:value=>"/Common/#{name}", :default_flag=>false}}
+      @bigip['LocalLB.ProfileHttpClass'].set_rewrite_url route_names.map {|name| "/Common/#{name}"}, paths.map{|path| {:value=>"[string map { \"#{path}\" \"/\" } [HTTP::uri]]", :default_flag=>false}}
+      @bigip['LocalLB.VirtualServer'].add_httpclass_profile ['ose-vlan'], [route_names.map {|name| {:profile_name=>"/Common/#{name}", :priority=>(priority += 1)}}]
     end
 
     def delete_routes pool_names, route_names
-      @bigip['LocalLB.VirtualServer'].remove_httpclass_profile ['ose-vlan'], [route_names.map {|n| {:profile_name=>n, :priority=>0}}]
-      @bigip['LocalLB.ProfileHttpClass'].delete_profile route_names
+      @bigip['LocalLB.VirtualServer'].remove_httpclass_profile ['ose-vlan'], [route_names.map {|n| {:profile_name=>"/Common/#{n}", :priority=>0}}]
+      @bigip['LocalLB.ProfileHttpClass'].delete_profile route_names.map {|name| "/Common/#{name}"}
     end
 
     def get_monitor_names
@@ -79,7 +79,7 @@ module OpenShift
     end
 
     def get_pool_members pool_name
-      @bigip['LocalLB.Pool'].get_member([pool_name])[0].collect do |pool_member|
+      @bigip['LocalLB.Pool'].get_member(["/Common/#{pool_name}"])[0].collect do |pool_member|
           pool_member['address'] + ':' + pool_member['port'].to_s
         end
     end
@@ -87,11 +87,11 @@ module OpenShift
     alias_method :get_active_pool_members, :get_pool_members
 
     def add_pool_members pool_names, member_lists
-      @bigip['LocalLB.Pool'].add_member pool_names, member_lists.map {|members| members.map {|address,port| { 'address' => address, 'port' => port }}}
+      @bigip['LocalLB.Pool'].add_member pool_names.map {|pool| "/Common/#{pool}"}, member_lists.map {|members| members.map {|address,port| { 'address' => address, 'port' => port }}}
     end
 
     def delete_pool_members pool_names, member_lists
-      @bigip['LocalLB.Pool'].remove_member pool_names, member_lists.map {|members| members.map {|address,port| { 'address' => address, 'port' => port }}}
+      @bigip['LocalLB.Pool'].remove_member pool_names.map {|pool| "/Common/#{pool}"}, member_lists.map {|members| members.map {|address,port| { 'address' => address, 'port' => port }}}
     end
 
     def authenticate host=@host, user=@user, passwd=@passwd
